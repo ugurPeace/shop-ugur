ee26fe1f812e050df785a0aec901465698239363
Ugur <ugurarslanpeace@gmail.com>
2024-02-29 22:54:29 +0300

    Feb 29

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,9 +1,26 @@
 import { motion } from "framer-motion";
 import "../../styles/product-card.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../redux/slices/cartSlice";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function ProductCard({ item }) {
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(
+      cartActions.addItem({
+        id: item.id,
+        productName: item.productName,
+        price: item.price,
+        image: item.imgUrl,
+      })
+    );
+    toast.success("Product added successfully");
+  };
+
   return (
     <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
@@ -18,7 +35,7 @@ function ProductCard({ item }) {
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
           <span className="price">${item.price}</span>
-          <motion.span whileTap={{ scale: 1.2 }}>
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
             <i class="ri-add-line"></i>
           </motion.span>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,26 @@ import ReactDOM from "react-dom/client";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
